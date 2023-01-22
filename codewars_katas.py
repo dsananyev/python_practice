f084d27919921b190cf312c620e360798fed0143
dsananyev <ds.ananyev@gmail.com>
2023-01-22 23:13:11 +0300

    6kyu kata

--- codewars_katas.py
+++ codewars_katas.py
@@ -84,3 +84,26 @@ def duplicate_encode(word):
             output += '('
     return output
 
+#5
+'''
+Given an array of integers, find the one that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+'''
+
+def find_it(seq):
+    for i in range(0,len(seq)):
+        count = 0
+        for j in range(0, len(seq)):
+            if seq[i] == seq[j]:
+                count +=1
+        if count % 2 != 0:
+            return seq[i]
+        
+            
